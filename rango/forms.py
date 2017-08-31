f61602bdbc0eae175a5e3b8a5d053b1c073f37d6
Han Xin <hanxin@sinap.ac.cn>
2017-08-31 22:14:20 +0800

    add custom templates tags

--- rango/forms.py
+++ rango/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
+
 
 class CategoryForm(forms.ModelForm):
+
     name = forms.CharField(max_length=128,
-                            help_text="Please enter the category name.")
+                           help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -11,8 +14,10 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name',)
+
         
 class PageForm(forms.ModelForm):
+
     title = forms.CharField(max_length=128,
                             help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
@@ -22,7 +27,7 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
-        # exclude... == fields = ('tilte', 'url', 'views') 
+        # exclude... == fields = ('title', 'url', 'views')
         
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -33,4 +38,17 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
             
             return cleaned_data
-        
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 from django.db import models
 
+
 class Category(models.Model):
+
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
@@ -18,8 +21,10 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
-        
+
+
 class Page(models.Model):
+
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
@@ -27,3 +32,13 @@ class Page(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
