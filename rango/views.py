19982f0828832132bc4c2303b903a375fe512349
Han Xin <hanxin@sinap.ac.cn>
2017-09-01 22:21:16 +0800

    add user authentication

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,9 @@
 from rango.forms import CategoryForm, PageForm, UserProfileForm, UserForm
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
 from rango.models import Category, Page
 
@@ -104,3 +108,37 @@ def register(request):
                   {'user_form': user_form,
                    'profile_form': profile_form,
                    'registered': registered})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/user_login.html', {})
+
+
+@login_required
+def user_logout(request):
+
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required
+def restricted(request):
+
+    return HttpResponse("Since you're logged in, you can see this text!")
